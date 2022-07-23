8d9742ae5fc59cdb0cc6862545e04b8f233ac740
Clément <cironclement.pro@gmail.com>
2022-07-24 00:46:03 +0200

    twitteur v0.1

--- twitteur/src/components/rightmenu/rightmenunav.jsx
+++ twitteur/src/components/rightmenu/rightmenunav.jsx
@@ -3,10 +3,54 @@ import styles from "./rightmenunav.module.css";
 export default function Rightmenunav() {
     return (
         <div className={styles.rightmenunavcontainer}>
+            <div className={styles.httitle}>
+                <p>Trends for you</p>
+            </div>
             <ul>
-                <li>hashtag1</li>
-                <li>hashtag2</li>
-                <li>hashtagz3</li>
+                <div className={styles.htcontainer}>
+                    <li className={styles.ht}>
+                        <a href="#">#React.JS</a>
+                        <a className={styles.underht} href="#">
+                            90K tweets
+                        </a>
+                    </li>
+                </div>
+
+                <div className={styles.htcontainer}>
+                    <li className={styles.ht}>
+                        <a href="#">#React.JS</a>
+                        <a className={styles.underht} href="#">
+                            90K tweets
+                        </a>
+                    </li>
+                </div>
+
+                <div className={styles.htcontainer}>
+                    <li className={styles.ht}>
+                        <a href="#">#React.JS</a>
+                        <a className={styles.underht} href="#">
+                            90K tweets
+                        </a>
+                    </li>
+                </div>
+
+                <div className={styles.htcontainer}>
+                    <li className={styles.ht}>
+                        <a href="#">#React.JS</a>
+                        <a className={styles.underht} href="#">
+                            90K tweets
+                        </a>
+                    </li>
+                </div>
+
+                <div className={styles.htcontainer}>
+                    <li className={styles.ht}>
+                        <a href="#">#React.JS</a>
+                        <a className={styles.underht} href="#">
+                            90K tweets
+                        </a>
+                    </li>
+                </div>
             </ul>
         </div>
     );
